104158de7bf00c6d274c4d98e7dc920efe5fdc2f
Rebeka23-git <158557757+Rebeka23-git@users.noreply.github.com>
2024-03-22 21:16:17 +0100

    updated HTML, added axios and injected APIs into js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generateJoke(event) {
-  event.preventDefault();
-
+function displayJoke(response) {
   new Typewriter("#joking", {
-    strings: "Generating joke for you..",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateJoke(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "400fcc7471aod0afddf69b6c3tfef4a0";
+  let prompt = `Generate a joke about ${instructionsInput.value}`;
+  let context =
+    "You are a funny Joke expert and love to tell jokes. Your mission is to generate a hilarious joke and make sure to follow the user instructions. Display only the joke. Sign the joke with `SheCodes AI` at the end of the joke.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let jokeElement = document.querySelector("#joking");
+  jokeElement.classList.remove("hidden");
+
+  jokeElement.innerHTML = `<div classs = "generating"> Generating a joke about ${instructionsInput.value}</div>`;
+  axios.get(apiUrl).then(displayJoke);
 }
 
 let jokeFormElement = document.querySelector("#jokes");
